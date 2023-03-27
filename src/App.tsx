6dc7bcc0e4726885b43adb5aa704ad4e98b01b74
Jaque <j.costa@dino.com.br>
2023-03-27 18:40:44 -0300

    adjutes

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,19 @@ import light from "./styles/themes/light";
 import MainComponents from "./documentation/main/main";
 import GlobalStyle from "./styles/globalStyle";
 import { HashRouter } from "react-router-dom";
+import usePeristedState from "./utils/usePersistedState";
 
 export default function App(props) {
-  const theme: any = light;
-  const { children } = props;
+    const theme: any = light;
+    const { children } = props;
 
-  return (
-    <ThemeProvider theme={theme}>
-      <HashRouter>
-      <GlobalStyle />
-        <MainComponents />
-        {children}
-      </HashRouter>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <HashRouter>
+                <GlobalStyle />
+                <MainComponents />
+                {children}
+            </HashRouter>
+        </ThemeProvider>
+    );
 }
